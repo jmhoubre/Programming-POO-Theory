f060e28fe0868cb7092bf3d15a3ebeed28686656
Jean-Michel HOUBRE <51229041+jmhoubre@users.noreply.github.com>
2022-02-22 20:17:41 +0100

    Ajout du NavMesh
    * Création objets statiques (Ground, House)
    * Ajout du NavMeshAgent

---
+++ POO Programming Theory/Assets/Scripts/Action.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Action
+{
+	public void Execute (Need _action)
+	{
+		Debug.Log ($"Execute {_action}");
+	}
+}
--- POO Programming Theory/Assets/Scripts/Human.cs
+++ POO Programming Theory/Assets/Scripts/Human.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Human : LivingBeing
 {
+	private List<Action> actions;
+
 	public override void Awake ()
 	{
 		base.Awake ();
+		actions = new List<Action> ();
 	}
 
 	public override void Start ()
